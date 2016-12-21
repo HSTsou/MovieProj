6ce787a5a740f04af3bb62f2333dc07f9ff0943e
HSTsou <aids71214@gmail.com>
2016-12-21 19:58:04 +0800

    modify databaseHelper(move DBcontants )

--- app/src/main/java/com/example/handsome/thenewtest/helper/DatabaseHelper.java
+++ app/src/main/java/com/example/handsome/thenewtest/helper/DatabaseHelper.java
@@ -14,62 +14,51 @@ import org.json.JSONObject;
 
 import java.util.Iterator;
 
-public class DatabaseHelper extends SQLiteOpenHelper implements DBConstants {
+import static com.android.volley.VolleyLog.TAG;
 
-    public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
 
-    public static final String[] BASIC_MOVIE_COLUMNS = {MOVIE.IMG_LINK,
-            MOVIE.MAIN_NAME, MOVIE.EN_NAME, MOVIE.ID};
+public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String[] COL_THEATER_LIST = {THEATER.NAME,
-            THEATER.ADDRESS, THEATER.ID, THEATER.LAT, THEATER.LNG};
-
-    public static final String[] COL_THEATER_SHOWTIMES_INFO = {TH_SHOWTIME.TH_ID,
-            TH_SHOWTIME.TIME_INFO_STR};
-    public static final String[] COL_THEATER_INFO = {THEATER.ID,THEATER.NAME,
-            THEATER.ADDRESS,THEATER.PHONE,THEATER.LAT,THEATER.LNG,};
+    static final String DATABASE_NAME = "MMDB";
+    static final int DATABASE_VERSION = 1;
     public static final String[] COL_MOVIE = {
-            MOVIE.AT_MOVIES_MV_ID,
-            MOVIE.PLAYING_DATE,
-            MOVIE.MAIN_NAME,
-            MOVIE.EN_NAME,
-            MOVIE.GATE,
-            MOVIE.IMG_LINK,
-            MOVIE.LENGTH,
-            MOVIE.DIRECTOR,
-            MOVIE.ACTOR,
-            MOVIE.STORY,
-            MOVIE.WRITER,
-            MOVIE.STATE,
-            MOVIE.MV_IMDB_URL,
-            MOVIE.MV_TOMATOES_URL,
-            MOVIE.IMDb_RATING,
-            MOVIE.TOMATOES_RATING
+            Movie.AT_MOVIES_MV_ID,
+            Movie.PLAYING_DATE,
+            Movie.MAIN_NAME,
+            Movie.EN_NAME,
+            Movie.GATE,
+            Movie.IMG_LINK,
+            Movie.LENGTH,
+            Movie.DIRECTOR,
+            Movie.ACTOR,
+            Movie.STORY,
+            Movie.WRITER,
+            Movie.STATE,
+            Movie.MV_IMDB_URL,
+            Movie.MV_TOMATOES_URL,
+            Movie.IMDb_RATING,
+            Movie.TOMATOES_RATING
     };
-
     public static final String[] COL_MOVIE_INFO = {
-            MOVIE.AT_MOVIES_MV_ID,
-            MOVIE.PLAYING_DATE,
-            MOVIE.MAIN_NAME,
-            MOVIE.EN_NAME,
-            MOVIE.GATE,
-            MOVIE.IMG_LINK,
-            MOVIE.LENGTH,
-            MOVIE.DIRECTOR,
-            MOVIE.ACTOR,
-            MOVIE.STORY,
-            MOVIE.WRITER,
-            MOVIE.STATE,
-            MOVIE.MV_IMDB_URL,
-            MOVIE.MV_TOMATOES_URL,
-            MOVIE.IMDb_RATING,
-            MOVIE.TOMATOES_RATING,
-            MOVIE.ALL_MV_TH_SHOWTIME_LIST,
-            MOVIE.YOUTUBE_URL_LIST
+            Movie.AT_MOVIES_MV_ID,
+            Movie.PLAYING_DATE,
+            Movie.MAIN_NAME,
+            Movie.EN_NAME,
+            Movie.GATE,
+            Movie.IMG_LINK,
+            Movie.LENGTH,
+            Movie.DIRECTOR,
+            Movie.ACTOR,
+            Movie.STORY,
+            Movie.WRITER,
+            Movie.STATE,
+            Movie.MV_IMDB_URL,
+            Movie.MV_TOMATOES_URL,
+            Movie.IMDb_RATING,
+            Movie.TOMATOES_RATING,
+            Movie.ALL_MV_TH_SHOWTIME_LIST,
+            Movie.YOUTUBE_URL_LIST
     };
-
     public static final String[] COL_MV_LIST = {
             DBConstants.MOVIE.STATE,
             DBConstants.MOVIE.AT_MOVIES_MV_ID,
@@ -79,16 +68,113 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DBConstants {
             DBConstants.MOVIE.IMG_LINK,
             DBConstants.MOVIE.IMDb_RATING,
             DBConstants.MOVIE.TOMATOES_RATING,
-            MOVIE.ID};
+            Movie.ID};
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    private static class Movie {
+        static final String TABLE_NAME = "Movie";
+        static final String ID = "id";
+        static final String PLAYING_DATE = "playingDate";
+        static final String AT_MOVIES_MV_ID = "atMoviesMvId";
+        static final String MAIN_NAME = "mvName";
+        static final String EN_NAME = "enName";
+        static final String GATE = "gate";
+        static final String IMG_LINK = "imgLink";
+        static final String LENGTH = "mvlength";//GAE ....mvLength
+        static final String DIRECTOR = "director";
+        static final String ACTOR = "actor";
+        // String LINK_LIST = "linkList";
+        static final String STORY = "story";
+        static final String UPDATE_DATE = "updateDate";
+        static final String MV_TIME_STR = "mvTimeStr";
+        static final String WRITER = "writer";
+        static final String STATE = "state";
+        static final String MV_IMDB_URL = "mv_IMDbMoblieUrl";
+        static final String MV_TOMATOES_URL = "mv_tomatoesMoblieUrl";//mobile in GAE was spelled wrong.
+        static final String IMDb_RATING = "IMDbRating";
+        static final String TOMATOES_RATING = "tomatoesRating";
+        static final String YOUTUBE_URL_LIST = "youtubeUrlList";
+        static final String ALL_MV_TH_SHOWTIME_LIST = "allMvThShowtimeList";
+
+        public static final String buildMovieSQL() {
+            StringBuilder s = new StringBuilder();
+            s.append(Movie.TABLE_NAME).append(" (")
+                    .append(Movie.ID).append(" text , ").append(Movie.MAIN_NAME)
+                    .append(" text not null, ").append(Movie.EN_NAME).append(" text, ").append(Movie.AT_MOVIES_MV_ID)
+                    .append(" text, ").append(Movie.GATE).append(" text, ").append(Movie.IMG_LINK).append(" text, ")
+                    .append(Movie.WRITER).append(" text, ")
+                    .append(Movie.LENGTH).append(" text, ").append(Movie.DIRECTOR).append(" text, ")
+                    .append(Movie.ACTOR).append(" text, ").append(Movie.MV_TIME_STR).append(" text, ")
+                    .append(Movie.STORY).append(" text, ")
+                    .append(Movie.PLAYING_DATE).append(" text, ").append(Movie.STATE).append(" text, ")
+                    .append(Movie.MV_IMDB_URL).append(" text, ").append(Movie.MV_TOMATOES_URL).append(" text, ")
+                    .append(Movie.IMDb_RATING).append(" real, ")
+                    .append(Movie.TOMATOES_RATING).append(" real, ")
+                    .append(Movie.YOUTUBE_URL_LIST).append(" text, ")
+                    .append(Movie.ALL_MV_TH_SHOWTIME_LIST).append(" text, ").append(Movie.UPDATE_DATE).append(" text, PRIMARY KEY (").append(Movie.ID).append(") );");
+            return s.toString();
+
+        }
+
+    }
+
+    private static class Theater {
+        static final String TABLE_NAME = "Theater";
+        static final String ID = "_id";
+        static final String NAME = "name";
+        static final String ADDRESS = "address";
+        static final String PHONE = "phone";
+        static final String AREA = "area";
+        static final String LAT = "lat";
+        static final String LNG = "lng";
+
+        public static final String buildTheaterSQL() {
+            StringBuilder s = new StringBuilder();
+            s.append(Theater.TABLE_NAME ).append(" (")
+                    .append(Theater.ID).append(" text primary key, ").append(Theater.NAME)
+                    .append(" text not null, ").append(Theater.ADDRESS).append(" text not null, ")
+                    .append(Theater.PHONE).append(" text, ").append(Theater.AREA).append(" text not null, ")
+                    .append(Theater.LAT).append(" text, ").append(Theater.LNG).append(" text);");
+            return s.toString();
+        }
+
+    }
+
+    private static class ThShowtime {
+        static final String TABLE_NAME = "Th_Showtime";
+        static final String TH_ID = "thId";
+        static final String TIME_INFO_STR = "timeInfoStr";
+        //String UPDATE_TIME= "updateTime";
+
+        public static final String buildThShowtimeSQL() {
+            StringBuilder s = new StringBuilder();
+            s.append(ThShowtime.TABLE_NAME).append(" (")
+                   .append(ThShowtime.TH_ID).append(" text primary key, ").append(ThShowtime.TIME_INFO_STR)
+                    .append(" text);");
+            return s.toString();
+        }
+
+    }
+
+    public static final String[] BASIC_MOVIE_COLUMNS = {Movie.IMG_LINK,
+            Movie.MAIN_NAME, Movie.EN_NAME, Movie.ID};
+    public static final String[] COL_THEATER_LIST = {Theater.NAME,
+            Theater.ADDRESS, Theater.ID, Theater.LAT, Theater.LNG};
+    public static final String[] COL_THEATER_SHOWTIMES_INFO = {ThShowtime.TH_ID,
+            ThShowtime.TIME_INFO_STR};
+    public static final String[] COL_THEATER_INFO = {Theater.ID, Theater.NAME,
+            Theater.ADDRESS, Theater.PHONE, Theater.LAT, Theater.LNG,};
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        Log.i("TEST", "DatabaseHelper creates tables");
-        db.execSQL("create table " + CREATE_MOVIE);
-        db.execSQL("create table " + CREATE_THEATER);
-        db.execSQL("create table " + CREATE_TH_SHOWTIME);
-
-
+        Log.i(TAG, "DatabaseHelper creates tables");
+        db.execSQL("create table " + Movie.buildMovieSQL());
+        db.execSQL("create table " + Theater.buildTheaterSQL());
+        db.execSQL("create table " + ThShowtime.buildThShowtimeSQL());
     }
 
     @Override
@@ -114,87 +200,86 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DBConstants {
     //20151108
     public long insertTheaterTimeInfo(SQLiteDatabase db, String thId,
                                       String timeInfoStr) {
-        Log.i("hs", "insertTheaterTimeInfo" );
+        Log.i("hs", "insertTheaterTimeInfo");
         ContentValues values = new ContentValues();
-        values.put(TH_SHOWTIME.TH_ID, thId);
-        values.put(TH_SHOWTIME.TIME_INFO_STR, timeInfoStr);
+        values.put(ThShowtime.TH_ID, thId);
+        values.put(ThShowtime.TIME_INFO_STR, timeInfoStr);
         //values.put(TH_SHOWTIME.UPDATE_TIME, updateTime);
-        return db.replace(TH_SHOWTIME.TABLE_NAME, null, values);
+        return db.replace(ThShowtime.TABLE_NAME, null, values);
     }
 
     //20151108
     public Cursor getTheaterTimeInfoById(SQLiteDatabase db, String thId) {
-        String WHERE = TH_SHOWTIME.TH_ID + " = ?";
+        String WHERE = ThShowtime.TH_ID + " = ?";
         String[] ARGS = {thId};
-        return db.query(TH_SHOWTIME.TABLE_NAME, COL_THEATER_SHOWTIMES_INFO, WHERE, ARGS,
+        return db.query(ThShowtime.TABLE_NAME, COL_THEATER_SHOWTIMES_INFO, WHERE, ARGS,
                 null, null, null);
     }
 
     //20151117
     public Cursor getTheaterInfoById(SQLiteDatabase db, String thId) {
-        String WHERE = THEATER.ID + " = ?";
+        String WHERE = Theater.ID + " = ?";
         String[] ARGS = {thId};
-        return db.query(THEATER.TABLE_NAME, COL_THEATER_INFO , WHERE, ARGS,
+        return db.query(Theater.TABLE_NAME, COL_THEATER_INFO, WHERE, ARGS,
                 null, null, null);
     }
 
     //20151117
     public Cursor getGaeIDByAtsMovieMvId(SQLiteDatabase db, String mvId) {
-        String[] array = {MOVIE.ID, MOVIE.AT_MOVIES_MV_ID};
-        String WHERE = MOVIE.AT_MOVIES_MV_ID + " = ?";
+        String[] array = {Movie.ID, Movie.AT_MOVIES_MV_ID};
+        String WHERE = Movie.AT_MOVIES_MV_ID + " = ?";
         String[] ARGS = {mvId};
-        return db.query(MOVIE.TABLE_NAME, array , WHERE, ARGS,
+        return db.query(Movie.TABLE_NAME, array, WHERE, ARGS,
                 null, null, null);
     }
 
     //20151110
     public long insertMovieInfo(SQLiteDatabase db, ContentValues values) {
-     //   Log.i("hs", " insertMovieInfo" );
-        return db.replace(MOVIE.TABLE_NAME, null, values);
+        //   Log.i("hs", " insertMovieInfo" );
+        return db.replace(Movie.TABLE_NAME, null, values);
     }
 
     //20151111
     public Cursor getMovieListData(SQLiteDatabase db, String orderBy, String state) {
         if (orderBy == null)
-            orderBy = MOVIE.PLAYING_DATE;
+            orderBy = Movie.PLAYING_DATE;
 
-        String WHERE = MOVIE.STATE + " = ?";
+        String WHERE = Movie.STATE + " = ?";
         String[] ARGS = {state};
 
-        return db.query(MOVIE.TABLE_NAME, COL_MV_LIST, WHERE, ARGS,
+        return db.query(Movie.TABLE_NAME, COL_MV_LIST, WHERE, ARGS,
                 null, null, orderBy + " DESC");
     }
 
     //20151111
     public Cursor getMovieInfoByAtsMovieId(SQLiteDatabase db, String mvId) {
 
-        String WHERE = MOVIE.ID + " = ?";
+        String WHERE = Movie.ID + " = ?";
         String[] ARGS = {mvId};
 
-        return db.query(MOVIE.TABLE_NAME, COL_MOVIE_INFO, WHERE, ARGS,
+        return db.query(Movie.TABLE_NAME, COL_MOVIE_INFO, WHERE, ARGS,
                 null, null, null);
     }
 
 
     public long insertTheater(SQLiteDatabase db, String[] theater, String area) {
         ContentValues values = new ContentValues();
-        values.put(THEATER.ID, theater[0]);
-        values.put(THEATER.NAME, theater[1]);
-        values.put(THEATER.ADDRESS, theater[2]);
-        values.put(THEATER.PHONE, theater[3]);
-        values.put(THEATER.LAT, theater[4]);
-        values.put(THEATER.LNG, theater[5]);
-        values.put(THEATER.AREA, area);
-        return db.insert(THEATER.TABLE_NAME, null, values);
+        values.put(Theater.ID, theater[0]);
+        values.put(Theater.NAME, theater[1]);
+        values.put(Theater.ADDRESS, theater[2]);
+        values.put(Theater.PHONE, theater[3]);
+        values.put(Theater.LAT, theater[4]);
+        values.put(Theater.LNG, theater[5]);
+        values.put(Theater.AREA, area);
+        return db.insert(Theater.TABLE_NAME, null, values);
     }
 
-      public Cursor getTheaters(SQLiteDatabase db, String area) {
-        String WHERE = THEATER.AREA + " = ?";
+    public Cursor getTheaters(SQLiteDatabase db, String area) {
+        String WHERE = Theater.AREA + " = ?";
         String[] ARGS = {area};
-        return db.query(THEATER.TABLE_NAME, COL_THEATER_LIST, WHERE, ARGS,
+        return db.query(Theater.TABLE_NAME, COL_THEATER_LIST, WHERE, ARGS,
                 null, null, null);
     }
-
 
 
 }
